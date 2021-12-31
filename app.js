d093cbed80adbc4a5deedfdf2e6aa015eee754ed
agus3gunawan <agus3gunawan@gmail.com>
2021-12-31 15:17:32 +0700

    update word 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.status(200).json('Hello world1')
+  res.status(200).json('Hello world')
 })
 
 module.exports = app.listen(4000, () => console.log(`Running on http://localhost:4000`))
